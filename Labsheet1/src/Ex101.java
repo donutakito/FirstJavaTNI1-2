5978b5e56a0c553013d04c98a74f9bb6b5c8506f
JustDonutwithMyMimi <victorakito303549@gmail.com>
2025-12-10 13:36:16 +0700

    2513110631 Lab01

---
+++ Labsheet1/src/Ex101.java
@@ -0,0 +1,5 @@
+public class Ex101 {
+    static void main() {
+        System.out.println("Yooo imma donut");
+    }
+}
---
+++ Labsheet1/src/Ex102.java
@@ -0,0 +1,5 @@
+public class Ex102 {
+    static void main() {
+        System.out.println("Hello,I am Donut\n\"INT-108\" \'Object-Oriented Programming\'\n\tThis is my first time learning Java\n\tI hope to get grade \"A\"");
+    }
+}
