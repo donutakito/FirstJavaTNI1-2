5d9889d41a0cc9edab68fa455a46718cda540a48
JustDonutwithMyMimi <victorakito303549@gmail.com>
2025-12-15 23:45:29 +0700

    Add files via upload

--- Labsheet05/src/Lab506.java
+++ Labsheet05/src/Lab506.java
@@ -1,5 +1,37 @@
-public class Lab506 {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.util.Scanner;
 
+public class Lab506 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Enter an integer:"));
+
+        while (number <= 1) {
+            number = Integer.parseInt(JOptionPane.showInputDialog("Number must be more 1 !!\n" +
+                    "Enter an integer again:"));
+        }
+
+        boolean findPrimeNum = is_prime_number(number);
+
+        if(findPrimeNum)
+        {
+            JOptionPane.showMessageDialog(null,number+" is Prime number");
+        }else
+        {
+            JOptionPane.showMessageDialog(null,number+" is NOT Prime number");
+        }
     }
+
+    static boolean is_prime_number(int number)
+    {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
